663a7d53e67633c0cbb809d5e625725d34137df1
Eudes_KODIA <67458711+Gael199@users.noreply.github.com>
2026-06-18 01:26:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ DATABASE_URL = st.secrets["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
-# DATABASE_URL = "postgresql://economic_ai_user:Qk0Zgpqhwrx7YXwo7FA93SKdaP15UzrB@dpg-d8ph2ia8qa3s73bsa0mg-a.oregon-postgres.render.com/economic_ai"
-
-# engine = create_engine(DATABASE_URL)
-
 
 @st.cache_data
 def load_data():
